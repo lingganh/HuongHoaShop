cacfd9ec5faf08071b57514f1bf85b7c19102ea6
Hoàng Diệu Linh <dieulinh120411@gmail.com>
2025-09-06 16:02:32 +0700

    first commit

---
+++ src/main/java/com/example/huonghoashop/HuongHoaShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.huonghoashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuongHoaShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuongHoaShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/huonghoashop/HuongHoaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.huonghoashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuongHoaShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
